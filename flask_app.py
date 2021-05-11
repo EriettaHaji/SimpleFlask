f98e128616cf08e989f8b3c269dc735523e5aa6b
EriettaHaji <79988688+EriettaHaji@users.noreply.github.com>
2021-05-11 18:30:12 +0200

    Create flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'input'
+def home():
+    return "test"
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    name = request.form['un']
+    return 'hello ' + name
+

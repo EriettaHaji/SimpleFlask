1fc8d8eae7e4e5a705fd91c11b3717717a48bb6c
EriettaHaji <79988688+EriettaHaji@users.noreply.github.com>
2021-05-12 17:32:31 +0200

    Create flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,18 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 import sqlite3
 
+@app.route('/create')
+def create():
+	con = sqlite3.connect('login.db')
+	cur = con.cursor()
+	cur.execute(	"""	CREATE TABLE Users(
+					Username VARCHAR(20) NOT NULL PRIMARY KEY,
+					Password VARCHAR(20) NOT NULL
+						  )
+			""")
+	con.commit()
+	return 'CREATE'
+
 @app.route('/')
 def home():
     return render_template("simple_form.html")

ab6a889655a6762dc189c14d18fc283c6199255a
EriettaHaji <79988688+EriettaHaji@users.noreply.github.com>
2021-05-12 17:24:43 +0200

    Create flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -10,7 +10,7 @@ def home():
 def insert():
     con = sqlite3.connect('login.db')
     cur = con.cursor()
-    cur.execute("INSERT INTO USERS (Username, Password) VALUES (?,?)
+    cur.execute("INSERT INTO USERS (Username, Password) VALUES (?,?),
                 (request.form['un'],request.form['pw']
     con.commit()
     return request.form['un'] + ' added'

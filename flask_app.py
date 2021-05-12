4e95b78d20edefae369dd7d9c1c4b52053a2d5b8
EriettaHaji <79988688+EriettaHaji@users.noreply.github.com>
2021-05-12 17:18:51 +0200

    Create flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,24 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
+import sqlite3
 
 @app.route('/')
 def home():
     return render_template("simple_form.html")
 
-@app.route('/hello', methods=['POST'])
-def hello():
-    name = request.form['un']
-    return 'hello ' + name
-
+@app.route('/insert', methods=['POST'])
+def insert():
+    con = sqlite3.connect('login.db')
+    cur = con.cursor()
+    cur.execute("INSERT INTO USERS (Username, Password) VALUES (?,?)
+                (request.form['un'],request.form['pw']
+    con.commit()
+    return request.form['un'] + ' added'
+                 
+@app.route('/select')
+def select():     
+    con = sqlite3.connect('login.db')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Users")
+    return str(cur.fetchall())            
+             
